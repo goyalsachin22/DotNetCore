4dd534645ffc9be39c6a9cb1b1651d62021b2550
Sachin Goyal <sachin.goyal@sap.com>
2023-01-13 01:45:53 +0530

    Select list validation

--- CoreMVCDemo/CoreMVCDemo/Models/Employee.cs
+++ CoreMVCDemo/CoreMVCDemo/Models/Employee.cs
@@ -11,6 +11,8 @@ namespace CoreMVCDemo.Models
         [Required]
         [Display(Name= "Office Email")]
         public string Email { get; set; }
-        public Dept  Department { get; set; }
+
+        [Required]
+        public Dept?  Department { get; set; }
     }
 }
